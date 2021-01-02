46552ec09d5e22e9a874c19ba301bc1f589e2d58
ekaterina-bobrova <76866640+ekaterina-bobrova@users.noreply.github.com>
2021-01-02 15:57:24 +0300

    Add files via upload

---
+++ vue homework 2/1/script.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+    el:'#app',
+    data:{
+        title:'Стража',
+        show: false,
+
+    },
+    methods:{
+        showText: function(){
+             this.show = !this.show
+        },
+        
+    },  
+})
