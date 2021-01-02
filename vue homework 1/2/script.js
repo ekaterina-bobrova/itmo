4f2ca61886257cf59cdcdba2323aaa439ad1fee4
ekaterina-bobrova <76866640+ekaterina-bobrova@users.noreply.github.com>
2021-01-02 15:26:23 +0300

    Add files via upload

---
+++ vue homework 1/2/script.js
@@ -0,0 +1,14 @@
+let guest = new Vue({
+    el:'#guest',
+    data:{
+        guest:{
+            name: 'Петр',
+            lastName:'Иванов',
+            age: 47,
+            avatar:'img/avatar.jpg',
+            login:'petruha',
+            email: 'petr.ivanov@mail.ru',
+            dataRegistration: '02.01.2021'
+        }
+    }
+})
