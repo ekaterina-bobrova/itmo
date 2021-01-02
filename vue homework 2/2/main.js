55721cc7f79f42031e4f4abbdcdf551151f64d33
ekaterina-bobrova <76866640+ekaterina-bobrova@users.noreply.github.com>
2021-01-02 14:21:28 +0300

    Add files via upload

---
+++ vue homework 2/2/main.js
@@ -0,0 +1,48 @@
+let menu = new Vue({
+    el:'#menu',
+    data:{
+        menu:{
+            home: 'Главная',
+            shop: 'Магазин',
+            about: 'О нас',
+            portfolio: 'Работы',
+        },
+
+        curentPage: 'home',
+    },
+    methods:{
+        showActivePage: function(e){
+             let page = e.target.getAttribute('data-menu');
+             return this.curentPage = page
+        }
+    },
+    components:{
+        home:{
+            template: `<div>
+                        <h1 class='title'>Главная</h1>
+                        <p>Магазин чего-нибудь очень нужного.</p>
+                      </div>`,
+        },
+            shop:{
+            template: `<div>
+                            <h1 class='title'>Магазин</h1>
+                            <p>Здесь будет можно что-то купить.</p>
+                            
+                        </div>`,
+        },
+            about:{
+            template: `<div>
+                            <h1 class='title'>О нас</h1>
+                            <p class='text'>Здесь мы рассказываем о том, какие мы классные</p>
+                        </div>`,
+        },
+            portfolio:{
+            template: `<div>
+                            <h1 class='title'>Работы</h1>
+                            <p  class='text'>Здесь можно посмотреть, что мы уже продали.</p>
+                        </div>`,
+        }
+        
+    },
+   
+})
